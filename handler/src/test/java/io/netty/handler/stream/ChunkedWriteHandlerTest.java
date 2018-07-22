b26f39fa0efeca39f38c3ca9a8930652b461877b
zhanglb <zhangliangbo233@163.com>
2018-07-22 14:26:54 +0800

    测试类代码编译错误修改

--- handler/src/test/java/io/netty/handler/stream/ChunkedWriteHandlerTest.java
+++ handler/src/test/java/io/netty/handler/stream/ChunkedWriteHandlerTest.java
@@ -223,7 +223,7 @@ public class ChunkedWriteHandlerTest {
         ch.checkException();
         assertTrue(ch.finish());
 
-        assertEquals(0, ch.readOutbound());
+        assertEquals(java.util.Optional.of(0), ch.readOutbound());
         assertNull(ch.readOutbound());
     }
 
